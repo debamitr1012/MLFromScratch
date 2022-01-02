f7653781e251d844634222adc11c53e1b3f9d961
Debayudh Mitra <78213067+debamitr1012@users.noreply.github.com>
2022-01-02 19:34:29 +0530

    Update linear_regression.py

--- LinearRegression/linear_regression.py
+++ LinearRegression/linear_regression.py
@@ -6,7 +6,7 @@ mu = []
 std = []
 
 def load_data(filename):
-	df = pd.read_csv("C:/Users/91983/Downloads/house_price_data.csv", sep=",", index_col=False)
+	df = pd.read_csv(filename, sep=",", index_col=False)
 	df.columns = ["housesize", "rooms", "price"]
 	data = np.array(df, dtype=float)
 	plot_data(data[:,:2], data[:, -1])
